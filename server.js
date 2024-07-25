78c225b369649bb75c21a78908ad5abe0f36185c
VaibhavDixit0909 <vaibhavdixitcse@gmail.com>
2024-07-25 14:16:55 +0530

    server file change

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const Dbconnect = require("./utils/Dbconnect");
 const routes = require("./routes/user.routes");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: "http://localhost:5173",
   optionsSuccessStatus: 200,
@@ -14,6 +15,6 @@ app.use(cors(corsOptions));
 Dbconnect();
 
 app.use("/api/v1/", routes);
-app.listen(4000, (req, res) => {
+app.listen(PORT, (req, res) => {
   console.log("APP running ");
 });
